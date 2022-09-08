fd88a02c0572220f693568b7157c3c3068f1f88f
Dmytro Sotnyk <dsotnyk@lacero.io>
2022-09-08 13:54:00 +0300

    refactoring

--- contracts/Soulbound.sol
+++ contracts/Soulbound.sol
@@ -2,10 +2,11 @@
 
 pragma solidity 0.8.16;
 
+import "./ISBT.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
-contract Soulbound is EIP712 {
+contract Soulbound is EIP712, ISBT {
     using Strings for uint256;
 
     address verifier;
@@ -22,27 +23,32 @@ contract Soulbound is EIP712 {
     }
 
     bytes32 private constant MINT_TYPEHASH =
-        keccak256(
-            "Mint(address verifier,address to,uint256 nonce)"
-        );
+        keccak256("Mint(address verifier,address to,uint256 nonce)");
 
-    mapping(address => uint256) nonces; 
+    mapping(address => uint256) nonces;
     uint256 public currentTokenId;
 
     // Mapping from token ID to owner address
     mapping(uint256 => address) private owners;
-    // Mapping owner address to token count
-    mapping(address => uint256) private balances;
+    // Mapping from owner address to his token
+    mapping(address => uint256) private tokens;
 
     // Emitted when a soulbound is issued to a soul
     event Minted(address indexed to, uint256 indexed tokenId);
-    // Emitted when a soulbound was burned from a soul
+    // Emitted when an issuer burns a soul
     event IssuerRevoke(address indexed from, uint256 indexed tokenId);
+    // Emitted when an owner burns a soul
+    event OwnerRevoke(address indexed from, uint256 indexed tokenId);
 
     // Checks if token actually exists
     modifier tokenExists(uint256 tokenId) {
-        require(_exists(tokenId), "KT: Token does not exist.");
+        require(_exists(tokenId), "SBT: Token does not exist.");
         _;
+    }
+
+    modifier onlyVerifier() {
+        require(msg.sender == verifier, "SBT: Sender is not verifier");
+        _; 
     }
 
     constructor(address _verifier, string memory _baseURI) EIP712(name, "1") {
@@ -50,46 +56,46 @@ contract Soulbound is EIP712 {
         baseURI = _baseURI;
     }
 
-    function mint(MintParams calldata params, bytes calldata signature) external {
-        require(params.nonce == nonces[msg.sender], "KT: invalid nonce");
-        require(params.to != address(0), "KT: mint to the zero address");
-        require(balances[params.to] == 0, "KT: Can have only one token.");
+    function mint(MintParams calldata params, bytes calldata signature)
+        external
+    {
+        require(params.nonce == nonces[msg.sender], "SBT: invalid nonce");
+        require(params.to != address(0), "SBT: mint to the zero address");
+        require(tokens[params.to] == 0, "SBT: Can have only one token.");
 
         bytes32 structHash = keccak256(
-            abi.encode(
-                MINT_TYPEHASH,
-                params.verifier,
-                params.to,
-                params.nonce
-            )
+            abi.encode(MINT_TYPEHASH, params.verifier, params.to, params.nonce)
         );
         bytes32 hash = _hashTypedDataV4(structHash);
         address signer = ECDSA.recover(hash, signature);
-        require(signer == verifier, "KT: Signer is not verifier");
+        require(signer == verifier, "SBT: Signer is not verifier");
 
-        unchecked {
-            balances[params.to] += 1;
-        }
+        tokens[params.to] = currentTokenId;
 
-        nonces[msg.sender]++;
         emit Minted(params.to, nonces[msg.sender]);
     }
 
-    function burn(uint256 tokenId) external tokenExists(tokenId) {
-        address owner = _ownerOf(tokenId);
-        balances[owner] -= 1;
-        delete owners[tokenId];
+    function burn() external {
+        address owner = msg.sender; 
+        uint256 tokenId = tokens[owner];
+        _burn(owner, tokenId);
+        emit OwnerRevoke(owner, tokenId);
+    }
 
+    function burnFrom(address owner) external onlyVerifier {
+        uint256 tokenId = tokens[owner];
+        _burn(owner, tokenId);
         emit IssuerRevoke(owner, tokenId);
     }
 
-    function balanceOf(address owner) external view returns (uint256) {
-        require(owner != address(0), "KT: Invalid owner");
-        return balances[owner];
+    function tokenOf(address owner) external view returns (uint256) {
+        require(owner != address(0), "SBT: Invalid owner");
+        return tokens[owner];
     }
 
     function ownerOf(uint256 tokenId)
         external
+        override
         view
         tokenExists(tokenId)
         returns (address)
@@ -110,11 +116,17 @@ contract Soulbound is EIP712 {
                 : "";
     }
 
-    function _ownerOf(uint256 tokenId) internal view virtual returns (address) {
+    function _burn( address owner, uint256 tokenId) internal {
+        require(tokenId != 0, "SBT: Cannot burn empty");
+        delete owners[tokenId];
+        delete tokens[owner];
+    }
+
+    function _ownerOf(uint256 tokenId) internal view returns (address) {
         return owners[tokenId];
     }
 
-    function _exists(uint256 tokenId) internal view virtual returns (bool) {
+    function _exists(uint256 tokenId) internal view returns (bool) {
         return _ownerOf(tokenId) != address(0);
     }
 }
